fd474c774ccb14d0d902649a0b337c96c0afccbf
Mokandu Minda <mindamokandu6@gmail.com>
2026-02-02 02:16:46 +0300

    Get DOM elements

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-
+const form = document.getElementById("nameForm");
+const result = document.getElementById("result");
+const dayInput = document.getElementById("day");
+const monthInput = document.getElementById("month");
+const yearInput = document.getElementById("year");
+const maleInput = document.getElementById("male");
+const femaleInput = document.getElementById("female");
 
 
 
