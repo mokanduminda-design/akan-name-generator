a1a6121e3a3d117e953c98e5872eaf4087f49b77
Mokandu Minda <mindamokandu6@gmail.com>
2026-02-03 15:11:28 +0300

    Formula

--- script.js
+++ script.js
@@ -27,7 +27,15 @@ function calculateAkanName() {
     const MM = month;
     const DD = day;
 
-   let d = Math.floor(d = ((CC/4 - 2*CC - 1) + (5*YY/4) + (26*(MM + 1)/10) + DD) % 7);
+   let d = (
+  Math.floor(CC / 4) -
+  2 * CC -
+  1 +
+  Math.floor(5 * YY / 4) +
+  Math.floor(26 * (MM + 1) / 10) +
+  DD
+) % 7;
+
    
     const maleNames = ["Kwame", "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi"];
     const femaleNames = ["Ama", "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua"];
